9b98cec05af675b8e65d4a4d9a999f2509728423
Luis Herranz <luisherranz@gmail.com>
2023-03-21 12:16:29 +0100

    Change : notation with .

--- src/blocks/example/render.php
+++ src/blocks/example/render.php
@@ -1,5 +1,5 @@
 <div <?php echo get_block_wrapper_attributes(); ?> data-wp-context='{ "show": false }'>
-	<button data-wp-on:click="actions.toggle" data-wp-effect="effects.logShow"> Toggle </button>
+	<button data-wp-on.click="actions.toggle" data-wp-effect="effects.logShow"> Toggle </button>
 	<div data-wp-show="context.show">
 		Some text!
 	</div>
